ceaeca46fe5081a707b12c7ae19ffcc72500b98d
Huda Yousif <hjdoski@gmail.com>
2019-10-13 20:16:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,10 @@
 def starts_with_a_vowel?(word)
-  if word.match 
+  if word.match (/^[AEIOU]|^[aeiou]/)
+    true 
+  else 
+    false 
+  end 
+end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
